b2ef8870bf563718afff77ff81a601d569384071
YueYuenu <fYAuXshPvAG9eHs>
2021-07-20 22:22:47 +0200

    user choice and start of morse to text

--- MorseTranslator/Program.cs
+++ MorseTranslator/Program.cs
@@ -9,6 +9,38 @@ namespace MorseTranslator
 
         static void Main()
         {
+            
+
+            Console.WriteLine("Press 1 to translate text to morse.");
+            Console.WriteLine("Press 2 to translate morse to text.");
+            var translatorMode = Console.ReadLine();
+
+            switch (translatorMode)
+            {
+                case "1":
+                    TextToMorse();
+                    break;
+
+                case "2":
+                    MorseToText();
+                    break;
+
+            }
+
+            /* if (translatorMode == "1");
+            //send to text to morse
+
+            else if (translatorMode == "2");
+            //send to morse to text 
+
+            else Console.WriteLine("You must press 1 or 2."); */
+
+        }
+
+        static void TextToMorse()
+        {
+            //following code is text to morse
+
             InitialiseDictionary();
 
             Console.WriteLine("Text to be translated here.");
@@ -19,16 +51,31 @@ namespace MorseTranslator
             for (int i = 0; i < input.Length; i++)
             {
                 if (i > 0)
+                {
                     Console.Write('/');
+                }
 
                 char c = input[i];
                 if (Morse.ContainsKey(c))
+                {
                     Console.Write(Morse[c]);
+                }
             }
 
             Console.WriteLine();
             Console.WriteLine("Press ESC to close this window.");
             while (Console.ReadKey().Key != ConsoleKey.Escape) { }
+        }
+
+        static void MorseToText()
+        {
+            //following code is morse to text
+
+            InitialiseDictionary();
+
+            Console.WriteLine("Text to be translated here.");
+            string input = Console.ReadLine();
+
         }
 
         private static void InitialiseDictionary()
